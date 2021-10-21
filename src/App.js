20ebcdfb9a73ef5407ef608b18580c6a3d7c2316
mujtaba1747 <mujtaba1747@gmail.com>
2021-10-22 00:51:54 +0530

    Added mint alert

--- src/App.js
+++ src/App.js
@@ -1,23 +1,154 @@
 import './styles/App.css';
+import { ethers } from "ethers";
 import twitterLogo from './assets/twitter-logo.svg';
-import React from "react";
+import React, { useEffect, useState } from "react";
+import myEpicNft from './utils/MyEpicNFT.json';
 
-// Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'mujtaba1747';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 // eslint-disable-next-line
 const OPENSEA_LINK = '';
 // eslint-disable-next-line
 const TOTAL_MINT_COUNT = 50;
 
+const CONTRACT_ADDRESS = "0x4CA26101D00Cc0C89E6C5F0eDfA916910bD7a78B";
+
 const App = () => {
-  // Render Methods
+
+    const [currentAccount, setCurrentAccount] = useState("");
+    
+    const checkIfWalletIsConnected = async () => {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+          console.log("Make sure you have metamask!");
+          return;
+      } else {
+          console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+      if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log("Found an authorized account:", account);
+          setCurrentAccount(account)
+
+          // Setup listener! This is for the case where a user comes to our site
+          // and ALREADY had their wallet connected + authorized.
+          setupEventListener()
+      } else {
+          console.log("No authorized account found")
+      }
+  }
+
+  /*
+  * Implement your connectWallet method here
+  */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      /*
+      * Fancy method to request access to account.
+      */
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      /*
+      * Boom! This should print out public address once we authorize Metamask.
+      */
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+
+
+      // Setup listener! This is for the case where a user comes to our site
+      // and connected their wallet for the first time.
+      setupEventListener();
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // Setup our listener.
+  const setupEventListener = async () => {
+      // Most of this looks the same as our function askContractToMintNft
+      try {
+        const { ethereum } = window;
+  
+        if (ethereum) {
+          // Same stuff again
+          const provider = new ethers.providers.Web3Provider(ethereum);
+          const signer = provider.getSigner();
+          const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+  
+          // THIS IS THE MAGIC SAUCE.
+          // This will essentially "capture" our event when our contract throws it.
+          // If you're familiar with webhooks, it's very similar to that!
+          connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+            console.log(from, tokenId.toNumber())
+            alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+          });
+  
+          console.log("Setup event listener!")
+  
+        } else {
+          console.log("Ethereum object doesn't exist!");
+        }
+      } catch (error) {
+        console.log(error)
+      }
+  }
+
+  const askContractToMintNft = async () => {
+      try {
+        const { ethereum } = window;
+  
+        if (ethereum) {
+          const provider = new ethers.providers.Web3Provider(ethereum);
+          const signer = provider.getSigner();
+          const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+  
+          console.log("Going to pop wallet now to pay gas...")
+          let nftTxn = await connectedContract.makeAnEpicNFT();
+  
+          console.log("Mining...please wait.")
+          await nftTxn.wait();
+          
+          console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+  
+        } else {
+          console.log("Ethereum object doesn't exist!");
+        }
+      } catch (error) {
+        console.log(error)
+      }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();// eslint-disable-next-line
+  }, [])
+
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
       Connect to Wallet
     </button>
   );
 
+  const renderMintUI = () => (
+    <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+      Mint NFT
+    </button>
+  )
+
+  /*
+  * Added a conditional render! We don't want to show Connect to Wallet if we're already conencted :).
+  */
   return (
     <div className="App">
       <div className="container">
@@ -26,7 +157,7 @@ const App = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {renderNotConnectedContainer()}
+          {currentAccount === "" ? renderNotConnectedContainer() : renderMintUI()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
@@ -35,7 +166,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
